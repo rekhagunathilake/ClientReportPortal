cefd1c67f8383e67dd7e0bf2958f379bef1ba943
raygunathilake <patience0112@gmail.com>
2026-09-21 23:06:04 +0530

    Add Create report package command and handler

---
+++ src/ClientReportPortal.Application/ReportPackages/CreateReportPackageHandler.cs
@@ -0,0 +1,15 @@
+﻿using ClientReportPortal.Domain.ReportPackages;
+using MediatR;
+
+namespace ClientReportPortal.Application.ReportPackages;
+
+public sealed class CreateReportPackageHandler(IReportPackageRepository repository)
+    : IRequestHandler<CreateReportPackageCommand, Guid>
+{
+    public async Task<Guid> Handle(CreateReportPackageCommand request, CancellationToken cancellationToken)
+    {
+        var package = ReportPackage.Create(request.ClientName, request.Quarter);
+        await repository.AddAsync(package, cancellationToken);
+        return package.Id;
+    }
+}
