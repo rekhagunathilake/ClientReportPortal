23fc5a1454358a4f407fae753c173c7ad1755c8e
raygunathilake <patience0112@gmail.com>
2026-09-21 23:06:29 +0530

    Add Application test project

---
+++ tests/ClientReportPortal.Application.Tests/FakeReportPackageRepository.cs
@@ -0,0 +1,15 @@
+﻿using ClientReportPortal.Application.ReportPackages;
+using ClientReportPortal.Domain.ReportPackages;
+
+namespace ClientReportPortal.Application.Tests;
+
+public sealed class FakeReportPackageRepository : IReportPackageRepository
+{
+    public ReportPackage? Added { get; private set; }
+
+    public Task AddAsync(ReportPackage package, CancellationToken ct)
+    {
+        Added = package;
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/ClientReportPortal.Application.Tests/ReportPackageTests.cs
@@ -0,0 +1,20 @@
+﻿using ClientReportPortal.Application.ReportPackages;
+using FluentAssertions;
+
+namespace ClientReportPortal.Application.Tests;
+public class ReportPackageTests
+{
+    [Fact]
+    public async Task Handle_CreatesPackageAndPersistsIt()
+    {
+        var repository = new FakeReportPackageRepository();
+        var handler = new CreateReportPackageHandler(repository);
+
+        var id = await handler.Handle(new CreateReportPackageCommand("Acme Wealth", "2026-Q3"), CancellationToken.None);
+
+        repository.Added.Should().NotBeNull();
+        repository.Added!.Id.Should().Be(id);
+        repository.Added.ClientName.Should().Be("Acme Wealth");
+        repository.Added.Quarter.Should().Be("2026-Q3");
+    }
+}
