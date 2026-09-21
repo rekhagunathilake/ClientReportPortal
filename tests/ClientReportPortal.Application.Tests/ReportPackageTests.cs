3ab40595a9ea2b8bc8a06d014e6550df6975a34b
raygunathilake <patience0112@gmail.com>
2026-09-21 23:13:33 +0530

    Add tests for submit section for review

--- tests/ClientReportPortal.Application.Tests/ReportPackageTests.cs
+++ tests/ClientReportPortal.Application.Tests/ReportPackageTests.cs
@@ -1,4 +1,5 @@
 ﻿using ClientReportPortal.Application.ReportPackages;
+using ClientReportPortal.Domain.ReportPackages;
 using FluentAssertions;
 
 namespace ClientReportPortal.Application.Tests;
@@ -16,5 +17,31 @@ public class ReportPackageTests
         repository.Added!.Id.Should().Be(id);
         repository.Added.ClientName.Should().Be("Acme Wealth");
         repository.Added.Quarter.Should().Be("2026-Q3");
+    }
+
+    // Submit section for review
+
+    [Fact]
+    public async Task Handle_MovesSectionToInReview()
+    {
+        var repository = new FakeReportPackageRepository();
+        var package = ReportPackage.Create("Acme Wealth", "2026-Q3");
+        var section = package.Sections.First(s => s.Type == SectionType.Performance);
+        repository.Seed(package);
+
+        var handler = new SubmitSectionForReviewHandler(repository);
+        await handler.Handle(new SubmitSectionForReviewCommand(package.Id, section.Id), CancellationToken.None);
+
+        repository.Saved!.Sections.Single(s => s.Id == section.Id).Status.Should().Be(SectionStatus.InReview);
+    }
+
+    [Fact]
+    public async Task Handle_ThrowsNotFoundWhenPackageDoesNotExist()
+    {
+        var repository = new FakeReportPackageRepository();
+        var handler = new SubmitSectionForReviewHandler(repository);
+
+        await Assert.ThrowsAsync<NotFoundException>(() =>
+            handler.Handle(new SubmitSectionForReviewCommand(Guid.NewGuid(), Guid.NewGuid()), CancellationToken.None));
     }
 }
