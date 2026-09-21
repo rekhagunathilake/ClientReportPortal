613a86a384f1db8e45ad0536c64961f58a5cae26
raygunathilake <patience0112@gmail.com>
2026-09-21 23:13:24 +0530

    Add command for submit section for review

---
+++ src/ClientReportPortal.Application/NotFoundException.cs
@@ -0,0 +1,3 @@
+﻿namespace ClientReportPortal.Application;
+
+public sealed class NotFoundException(string message) : Exception(message);
---
+++ src/ClientReportPortal.Application/ReportPackages/SubmitSectionForReviewCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace ClientReportPortal.Application.ReportPackages;
+
+public sealed record SubmitSectionForReviewCommand(Guid ReportPackageId, Guid SectionId) : IRequest;
